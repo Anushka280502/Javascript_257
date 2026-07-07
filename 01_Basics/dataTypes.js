c77d94b2491edd47dffc5388a2c8bd59170a0e3c
Anushka Gupta <anushkagupta0895@gmail.com>
2026-07-07 08:20:54 +0000

    DataTypes

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,40 @@
+"use strict"       //treat all JS code as newer version
+
+//alert( 3 + 3)  //we are using nodejs, not browser
+
+//code readability should be high (; can be used or can't be )
+
+//original documentation of js-----> ECMA writes standards for js
+//second one is mdn --- easier 
+
+
+let name = "anushka"    //string
+let age = 18            //numbers
+let isLoggedIn = false  //boolean
+let state;              //undefined
+
+//PRIMITIVE DATA TYPES
+
+//number => 2 to power 53
+//bigint for large numbers
+
+//string => ""
+
+//boolean => true/false
+
+//null => standalone value  //representation of empty value
+
+//undefined => when value is not defined
+
+//symbol =>  to find uniqueness
+
+
+
+//OBJECT
+
+console.log(typeof "anushka");        //string
+
+console.log(typeof null);             //object
+
+console.log(typeof undefined);        //undefined
+
